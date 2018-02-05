0434e282a36138f72df23953d6ce5409c2cfdba7
chengyongjun <chengyongjun@corp.netease.com>
2018-02-05 19:45:20 +0800

    Initial commit

---
+++ src/main/java/how2j/middle/io/TestStream.java
@@ -0,0 +1,16 @@
+package how2j.middle.io;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by joke
+ * 2018/2/5
+ * 字节流
+ */
+public class TestStream {
+
+    @Test
+    public void read(){
+        
+    }
+}
